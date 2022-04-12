2a0f376ead4c5dd3f3f7bc7d7475aaf70c4bca95
rachell <dazzlemevanity@gmail.com>
2022-04-11 22:34:40 -0400

    commit tree a9f7a4f4abfb76f3013ec2c2c020a3ea2ad521b1

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+// // const name = "Joe";
+// // const height = 74;
+// // const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+// // module.exports = {name, height, message};
+
+// function introduction (name){
+
+//     return `Hi, my name is ${name}.`;
+//     }
+
+//     function introductionWithLanguage (name,language) {
+
+//     return `Hi, my name is ${name} and I am learning to program in ${language}.`
+//     }
+//     function sayHelloTo(User) {
+//        return `Hello, ${firstName}!`;
+//       }
+      
+      
+      1 + 80; //=> 81
+      60 - 40; //=> 20
+      2 * 3.4; //=> 6.8
+      5.0 / 2.5; //=> 2
+
+
+      function add()
--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,51 @@
-const { name, height, message } = require("../index.js");
-
-/*
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
-    })
-  })
+// const { name, height, message } = require("../index.js");
 
 
-  describe('Height', () => {
-    it('is less than 40', () => {
-      expect(height).toBeLessThan(40)
-    })
-  })
+// describe('what-is-a-test', () => {
+//   describe('Name', () => {
+//     it('returns "Susan"', () => {
+//       expect(name).toEqual('Susan')
+//     })
+//   })
 
-  describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(message).toInclude(name)
-      expect(message).toInclude(height)
-    })
+
+//   describe('Height', () => {
+//     it('is less than 40', () => {
+//       expect(height).toBeLessThan(40)
+//     })
+//   })
+
+//   describe('Message', () => {
+//     it('gives the name and height', () => {
+//       expect(message).toInclude(name)
+//       expect(message).toInclude(height)
+//     })
+//   })
+// })
+
+describe('introduction(name)', function() {
+  it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
+    expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
+    expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
   })
 })
-*/
+
+describe('introductionWithLanguage(name, language)', function() {
+  it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
+    expect(introductionWithLanguage("Aki", "Ember.js")).toEqual("Hi, my name is Aki and I am learning to program in Ember.js.");
+    expect(introductionWithLanguage("Samip", "React")).toEqual("Hi, my name is Samip and I am learning to program in React.");
+  })
+})
+
+describe('introductionWithLanguageOptional(name, language)', function() {
+  it('takes in two arguments, a name and a language, and language defaults to JavaScript', function() {
+    expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in JavaScript.");
+  })
+})
+
+describe('introductionWithLanguageOptional(name, language)', function() {
+  it('takes in two arguments, a name and a language, and the default value can be overridden with an argument', function() {
+    expect(introductionWithLanguageOptional("Gracie", "Python")).toEqual("Hi, my name is Gracie and I am learning to program in Python.");
+  })
+})
+
